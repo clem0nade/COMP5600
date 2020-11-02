62912c52326199e4a09de3a2a7dd47d1e354f75e
Everett Clemons <eclem22@gmail.com>
2020-11-02 03:15:00 -0600

    Added stacked bar chart

--- app.py
+++ app.py
@@ -1,18 +1,94 @@
-import pandas
+import pandas as pd
 import plotly.graph_objects as go
 import plotly.subplots as sp
 import plotly.express as px
 import dash
+import dash_bio as dashbio
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import sqlite3
 from sqlite3 import Error
 import pyodbc
+from sklearn.cluster import KMeans
+import numpy
 
 #Globals
 mapbox_access_token = "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNrOWJqb2F4djBnMjEzbG50amg0dnJieG4ifQ.Zme1-Uzoi75IaFbieBDl3A"
 mapbox_style = "mapbox://styles/plotlymapbox/cjvprkf3t1kns1cqjxuxmwixz"
+stateCodes = [
+"AK",
+"AL",
+"AR",
+"AZ",
+"CA",
+"CO",
+"CT",
+"DC",
+"DE",
+"FL",
+"GA",
+"HI",
+"IA",
+"ID",
+"IL",
+"IN",
+"KS",
+"KY",
+"LA",
+"MA",
+"MD",
+"ME",
+"MI",
+"MN",
+"MS",
+"MO",
+"MT",
+"NC",
+"NE",
+"NH",
+"NJ",
+"NM",
+"NV",
+"NY",
+"ND",
+"OH",
+"OK",
+"OR",
+"PA",
+"RI",
+"SC",
+"SD",
+"TN",
+"TX",
+"UT",
+"VT",
+"VA",
+"WA",
+"WV",
+"WI",
+"WY"]
+
+def getAllAgeRates(connection, namesIn):
+    sql_Select = "select * from Age"
+    cursor = connection.cursor()
+    cursor.execute(sql_Select)
+    records = cursor.fetchall()
+    age1 = []
+    age2 = []
+    age3 = []
+    age4 = []
+
+    for row in records:
+        for name in namesIn:
+            if(name == row[0]):
+                age1.append(row[3]/row[2])
+                age2.append(row[4]/row[2])
+                age3.append(row[5]/row[2])
+                age4.append(row[6]/row[2])
+       
+    
+    return age1, age2, age3, age4
 
 def sqlConnect(path):
     connection = None
@@ -29,7 +105,7 @@ def fetchAge(connection):
     cursor = connection.cursor()
     cursor.execute(sql_Select)
     records = cursor.fetchall()
-
+    
     return records
 
 def fetchEducation(connection):
@@ -429,14 +505,14 @@ dash_app.layout = html.Div(
         
         html.Div(
             id = "app-container",
-            style = {'color' : '#1f2630', 'width':'49%','display': 'inline-block', 'vertical-align': 'middle'},
+            style = {'color' : '#1f2630', 'width':'49%','display': 'inline-block', 'vertical-align': 'top'},
             children=[
                 html.Div(
                     id = "left-column",
                     children=[
                         html.P(
                             id = "dropdown-text",
-                            children="Choose a demographic from the dropdown",
+                            children="Choose a Demographic from the Dropdown",
                             style = {'color': "#2cfec1"},
                         ),
                         dcc.Dropdown(id="map_select",
@@ -474,9 +550,18 @@ dash_app.layout = html.Div(
         ),
         html.Div(
             id="cluster-container",
-            style= {'color' : '#1f2630','width':'49%','display': 'inline-block', 'vertical-align': 'middle'},
+            style= {'color' : '#1f2630','width':'49%','display': 'inline-block', 'vertical-align': 'top'},
             children=[
-            html.P(id="cluster-label", children="Select Data on Right"),
+            html.P(id="cluster-label", children="Select Data on Right and Choose Graph Style Below", style = {'color': "#2cfec1"}),
+            dcc.Dropdown(
+                id="graph-select",
+                options=[
+                    {"label": "Line Graph", "value" : "line"},
+                    {"label" : "Stacked Bar Chart", "value": "cluster"}],
+                multi=False,
+                value = "line",
+                style={"backgroundColor": '#1f2630','width':'75%','color': "#2cfec1"},
+            ),
             dcc.Graph(
                 id="cluster-graph",
                 figure={}
@@ -503,10 +588,13 @@ def update_Graph(selected, figure):
 
 @dash_app.callback(
     Output('cluster-graph', 'figure'),
-    [Input(component_id='map',component_property='selectedData')],
+    [Input(component_id='map',component_property='selectedData'),
+    Input("graph-select", "value")],
     [State('cluster-graph', 'figure')]
 )
-def displaySelectedData(selectedData, figure):
+def displaySelectedData(selectedData, selectedGraph, figure):
+    connection = sqlConnect("CensusData.db")
+    print("Selected Graph is " + selectedGraph)
     if selectedData is None:
         return dict(
             data=[dict(x=0, y=0)],
@@ -526,16 +614,42 @@ def displaySelectedData(selectedData, figure):
         name.append(point['location'])
     print(name)
     print(z)
-    return dict(
-            data=[dict(x=name, y=z)],
-            layout=dict(
-                title="Voter Rates of Selected States",
-                paper_bgcolor="#1f2630",
-                plot_bgcolor="#1f2630",
-                font=dict(color="#2cfec1"),
-                margin=dict(t=75, r=50, b=100, l=75),
-            ),
+    if (selectedGraph == 'line'):
+        return dict(
+                data=[dict(x=name, y=z)],
+                layout=dict(
+                    title="Voter Rates of Selected States",
+                    paper_bgcolor="#1f2630",
+                    plot_bgcolor="#1f2630",
+                    font=dict(color="#2cfec1"),
+                    margin=dict(t=75, r=50, b=100, l=75),
+                ),
+            )
+    elif(selectedGraph == 'cluster'):
+        age1, age2, age3, age4 = getAllAgeRates(connection, name)
+        data = {}
+        data['State'] = name
+        data['19-29'] = age1
+        data['30-44'] = age2
+        data['45-64'] = age3
+        data['65+'] = age4
+        df = pd.DataFrame(data)
+        print(df)
+        fig = px.bar(df, x = 'State', 
+                    y = ['19-29', '30-44', '45-64', '65+'], 
+                    title="Stacked Bar Chart Voter Rates of Selected States",)
+        fig.update_layout(
+            paper_bgcolor="#1f2630",
+            plot_bgcolor="#1f2630",
+            font=dict(color="#2cfec1"),
+            margin=dict(t=75, r=50, b=100, l=75),
         )
+        return fig
+        
+
+
+
+
 
 if __name__ == "__main__":
     dash_app.run_server (host='0.0.0.0', port='80')
