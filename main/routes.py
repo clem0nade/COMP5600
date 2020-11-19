1887016da5ae23bc403faa0c6fb296fec6dd8e27
Everett Clemons <eclem22@gmail.com>
2020-11-19 02:06:23 -0600

    Added pyodbc functionality

--- main/routes.py
+++ main/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect
 from flask import current_app as app
-#from .dashboard import db
+import pyodbc
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -11,15 +11,19 @@ def index():
         gender = request.form['gender']
         degree = request.form['degree']
         # Process the above values in conjunction with voter data
-
-        # conn = db.sqlConnect()
-        # curs = conn.cursor()
-        # sql = "INSERT INTO Users Values (?, ?, ?, ?)"
-        # values = (name, age, gender, degree)
-        # curs.execute(sql, values)
-        # conn.commit()
-        # curs.close()
-        # conn.close()
+        server = 'tcp:comp5600.database.windows.net'
+        database = 'UserData'
+        username = '16-CAM'
+        password = 'COMP5600Database'
+        conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
+                              server+';DATABASE='+database+';UID='+username+';PWD=' + password)
+        curs = conn.cursor()
+        sql = "INSERT INTO Users Values (?, ?, ?, ?)"
+        values = (name, age, gender, degree)
+        curs.execute(sql, values)
+        conn.commit()
+        curs.close()
+        conn.close()
         # After processing, take users to the dash app
         # This route is defined directly in Dash's initialization
         return redirect("/cluster")
